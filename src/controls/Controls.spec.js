622ef08e19be42fd164357fe4cc70d8ebda7ab90
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-09-15 19:20:47 -0500

    basic unit tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,31 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+// import { render, fireEvent } from '@testing-library/react';
+import Controls from './Controls'
+import { render, fireEvent } from 'react-testing-library';
+
+describe('<Controls/>', () => {
+    it('should take a snapshot', () => {
+        const snap = renderer.create(<Controls />).toJSON();
+        //Jest
+        expect(snap).toMatchSnapshot()
+    })
+    it('should execute a function', () => {
+        const mock = jest.fn()
+        const { getByTestId } = render(<Controls toggleClosed={mock} />)
+
+        const button = getByTestId(/toggleClosed/i)
+        // console.log(button)
+        fireEvent.click(button)
+
+        expect(mock).toHaveBeenCalled()
+
+
+
+
+
+
+
+    })
+})
+
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,42 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
+import Dashboard from './Dashboard'
+
+describe('<Dashboard/>', () => {
+    it('should take a snapshot', () => {
+        const snap = renderer.create(<Dashboard />).toJSON();
+        //Jest
+        expect(snap).toMatchSnapshot()
+    })
+    it('should render open text', () => {
+        const Dash = render(<Dashboard />)
+        //Testing Library
+        Dash.getByText(/open/i)
+    })
+    it('should show a closed gate', () => {
+
+        const { getByText } = render(<Dashboard />)
+
+        const button = getByText(/close gate/i)
+
+        fireEvent.click(button)
+
+        getByText(/closed/i)
+    })
+    it('should show a locked gate', () => {
+
+        const { getByText } = render(<Dashboard />)
+
+        const Closebutton = getByText(/close gate/i)
+        const Lockbutton = getByText(/lock gate/i)
+
+        fireEvent.click(Closebutton)
+        fireEvent.click(Lockbutton)
+
+        getByText(/locked/i)
+
+    })
+})
+
+
